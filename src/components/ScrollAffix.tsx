3b4fd7bbe50a790598a2551e73483d7fa3601223
battagel <matthew@battagel.me.uk>
2022-12-21 23:07:22 +0000

    Scroll affix light/dark

--- src/components/ScrollAffix.tsx
+++ src/components/ScrollAffix.tsx
@@ -1,21 +1,25 @@
-import { Affix, Transition, Button } from "@mantine/core";
+import { Affix, Transition, Button, useMantineTheme } from "@mantine/core";
 import { useWindowScroll } from "@mantine/hooks";
 import { ArrowBigUpLine } from "tabler-icons-react";
 
 export default function ScrollAffix() {
   const [scroll, scrollTo] = useWindowScroll();
+
+  const theme = useMantineTheme();
+
   return (
     <Affix position={{ bottom: 20, right: 20 }}>
       <Transition transition="slide-up" mounted={scroll.y > 0}>
         {(transitionStyles) => (
           <Button
             leftIcon={
-              <ArrowBigUpLine size={20} strokeWidth={2} color={"white"} />
+              <ArrowBigUpLine size={20} strokeWidth={2} />
             }
             style={transitionStyles}
+            variant="light"
             onClick={() => scrollTo({ y: 0 })}
           >
-            Scroll to top
+            Back to Start
           </Button>
         )}
       </Transition>
